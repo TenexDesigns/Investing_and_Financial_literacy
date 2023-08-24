275633b85a64e24bf31f12776957a66c475ae4e0
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-24 21:10:48 +0300

    Create investing.java

---
+++ Build_your_Finaces or Money/investing.java
@@ -0,0 +1,3 @@
+different income-generating avenues,such as dividend-paying stocks, bonds, and
+certain types of real estate, Real estate investment ,Entrepreneurship, investing in income-generating assets,
+, starting a side business,assets that have the potential to grow over time,include stocks, bonds, mutual funds, real estate, and more.
